ce702713a1d5b9885996276ff5789be83c8d137b
Taylor J. Ampatiellos <Taylor.Ampatiellos@tufts.edu>
2016-10-29 15:16:55 -0400

    Added Pickup Mechanic

---
+++ Experiments/Experimenting with Rooms/Assets/_scripts/Magnetic.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Magnetic : MonoBehaviour
+{
+	public LayerMask m_MagneticLayers;
+	public Vector3 m_Position;
+	float m_Radius = 10;
+	float m_Force = 0;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.KeypadPlus) == true) {
+			print ("Hello");
+			m_Force = 50;
+		}
+
+		if (Input.GetKeyDown (KeyCode.KeypadMinus) == true) {
+			print ("Hello");
+			m_Force = -50;
+		}
+	}
+
+	void FixedUpdate () 
+	{
+		Collider[] colliders;
+		Rigidbody rigidbody;
+
+		colliders = Physics.OverlapSphere (transform.position + m_Position, m_Radius, m_MagneticLayers);
+		foreach (Collider collider in colliders) {
+			rigidbody = (Rigidbody)collider.gameObject.GetComponent (typeof(Rigidbody));
+			if (rigidbody == null) {
+				continue;
+			}
+			rigidbody.AddExplosionForce (m_Force * -1, transform.position + m_Position, m_Radius);
+		}
+
+	}
+
+	void OnDrawGizmosSelected ()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere (transform.position + m_Position, m_Radius);
+	}
+}
