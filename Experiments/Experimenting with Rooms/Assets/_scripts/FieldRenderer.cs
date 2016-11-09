2f4e52414daab15c4cd57e32f4dd4b4f2d4e65a5
Chris Hinstorff <chinst01@eecs.tufts.edu>
2016-11-08 20:46:31 -0500

    Add scene-level magnetic field rendering object

---
+++ Experiments/Experimenting with Rooms/Assets/_scripts/FieldRenderer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldRenderer : MonoBehaviour
+{
+	public bool RenderMagneticFields = false;
+
+	void Start ()
+	{
+		updateMeshRenderers ();
+	}
+
+	void OnGUI ()
+	{
+		updateMeshRenderers ();
+	}
+
+	private void updateMeshRenderers ()
+	{
+		MagneticField[] fields = FindObjectsOfType (typeof(MagneticField)) as MagneticField[];
+		foreach (MagneticField field in fields) {
+			field.gameObject.GetComponent<MeshRenderer> ().enabled = RenderMagneticFields;
+		}
+	}
+}
