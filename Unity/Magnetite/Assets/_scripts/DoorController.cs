1d7c494256e49fc93e85bb13baeb82b3a71eaf29
Stuart J.. Montgomery <smontg01@eecs.tufts.edu>
2016-11-22 03:48:15 -0500

    Script for buttons to open doors

---
+++ Unity/Magnetite/Assets/_scripts/DoorController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+	public GameObject Door;
+	public bool IsOpening;
+
+	void Update(){
+		if(IsOpening == true){
+			Door.transform.Translate(Vector3.up*Time.deltaTime*10);
+		}
+		if(Door.transform.position.y > 10f){
+			IsOpening = false; 
+		}
+	}
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.gameObject.tag == "Magnetic")
+			IsOpening = true;
+	}    
+}
