1247b6b961dced63e4b0bef0acd4890626edea0d
Taylor Ampatiellos <Taylor1VT@comcast.net>
2016-11-26 10:40:29 -0500

    Lasersssssss
    Lasers have been implemented in Level 1, Room 5!
    
    There is currently a script on the PlayerController (LaserReset) that
    teleports the player to a Vector3 location upon contact with a trigger
    with the tag "Laser." Currently, this teleport is fixed to a specific,
    in-code location. What needs to be done now is greating checkpoint
    triggers that update this Vector3 as the player goes through the levels
    (so that a laser three rooms from now doesn't teleport the player all
    the way back).

---
+++ Unity/Magnetite/Assets/_scripts/LaserReset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserReset : MonoBehaviour {
+
+	// Use this for initialization
+
+	
+		void OnTriggerEnter(Collider other) {
+			if(other.gameObject.tag == "Laser") { //Or whatever
+				transform.position = new Vector3(-7, 8, -73);
+			}
+
+		}
+
+	
+}
