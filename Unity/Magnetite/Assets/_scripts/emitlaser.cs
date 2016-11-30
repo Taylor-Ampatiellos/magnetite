dde55f3d81b3e28acf1b93fad882b0d168282a3d
Taylor Ampatiellos <Taylor1VT@comcast.net>
2016-11-30 10:39:56 -0500

    Began Experimenting with more Maleable Lasers
    Working on making lasers of variable length, that go until they hit
    something (instead of being a set length)

---
+++ Unity/Magnetite/Assets/_scripts/emitlaser.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class emitlaser : MonoBehaviour {
+
+	public float weaponRange = 50f;
+	public Transform gunEnd;
+
+	private LineRenderer laserLine;
+
+	void Start () {
+		laserLine = GetComponent<LineRenderer> ();
+	}
+
+	void Update () {
+			Vector3 rayOrigin = new Vector3 (5, 5, 5);
+			RaycastHit hit;
+
+			laserLine.SetPosition (0, gunEnd.position);
+
+			if (Physics.Raycast (rayOrigin, transform.forward, out hit, weaponRange)) {
+				laserLine.SetPosition (1, hit.point);
+			} else {
+				laserLine.SetPosition (1, rayOrigin + (transform.forward * weaponRange));
+			}
+	}
+}
