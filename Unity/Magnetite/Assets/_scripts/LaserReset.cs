738f6a985267080d93e3ec252135aa9d97f4dc77
Taylor J. Ampatiellos <Taylor.Ampatiellos@tufts.edu>
2016-11-29 19:53:34 -0500

    Checkpoints Now Reset Puzzles
    Still undergoing minor adjustments -- it is currently impossible to
    begin the game anywhere but the beginning (for testing purposes we want
    to be able to move the playercontroller to start anywhere).

--- Unity/Magnetite/Assets/_scripts/LaserReset.cs
+++ Unity/Magnetite/Assets/_scripts/LaserReset.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class LaserReset : MonoBehaviour {
 
-	Vector3 check;
-		
+	GameObject pos;
+
+	void Start() {
+		pos = GameObject.FindGameObjectWithTag("PlayerPosition");
+		transform.position = pos.GetComponent<playerposition> ().check;
+	}
+
 	void OnTriggerEnter(Collider other) {
+		DontDestroyOnLoad (pos);
+
 		if (other.gameObject.tag == "Laser") { //Or whatever
-			transform.position = check;
+			transform.position = pos.GetComponent<playerposition> ().check;
+			Scene scene = SceneManager.GetActiveScene ();
+			SceneManager.LoadScene (scene.name);
 		}
 
 		if (other.gameObject.tag == "checkpoint") { //Or whatever
-			check = other.transform.position;
+			pos.GetComponent<playerposition> ().check = other.transform.position;
 		}
 	}
 }
---
+++ Unity/Magnetite/Assets/_scripts/spawnpoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnpoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+}
