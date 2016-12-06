2bf5dd84cca2d7f2f794e854f040dcf1086e0be4
Chris Hinstorff <chinst01@eecs.tufts.edu>
2016-12-06 17:33:26 -0500

    Go to level 2 after level 1 is done

---
+++ Unity/Magnetite/Assets/_scripts/GoToLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoToLevel : MonoBehaviour
+{
+	public string targetLevelName;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			SceneManager.LoadScene (targetLevelName);
+			Scene s = SceneManager.GetSceneByName (targetLevelName);
+			playerposition pos = GameObject.FindGameObjectWithTag ("PlayerPosition").GetComponent<playerposition> ();
+			pos.spawn = new Vector3 (0, 0, 6);
+			SceneManager.SetActiveScene (s);
+		}
+	}
+
+}
