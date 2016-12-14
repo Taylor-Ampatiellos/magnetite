bd5508259bffd6645109857115b59f8915d39935
Taylor Ampatiellos <Taylor1VT@comcast.net>
2016-12-14 16:07:53 -0500

    Platforms can now be sticky BUT walking on them is impossible.
    In order to change position on a platform the player must jump and
    reorient themself.

--- Unity/Magnetite/Assets/_scripts/LaserReset.cs
+++ Unity/Magnetite/Assets/_scripts/LaserReset.cs
@@ -5,7 +5,13 @@ using System.Collections;
 public class LaserReset : MonoBehaviour {
 
 	GameObject pos;
-	Collider collider;
+	Collider collider = null;
+	float plat_x;
+	float plat_z;
+	float plat_change_x;
+	float plat_change_z;
+	float play_x;
+	float play_z;
 
 	void Start() {
 		pos = GameObject.FindGameObjectWithTag("PlayerPosition");
@@ -14,22 +20,48 @@ public class LaserReset : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		collider = other;
 		DontDestroyOnLoad (pos);
 
 		if (other.gameObject.tag == "Laser") { 
 			Scene scene = SceneManager.GetActiveScene ();
 			SceneManager.LoadScene (scene.name);
+			collider = other;
 		}
 
 		if (other.gameObject.tag == "checkpoint") { 
 			pos.GetComponent<playerposition> ().spawn = other.transform.position;
+			collider = other;
+		}
+
+		if (other.gameObject.tag == "Platform") { 
+			collider = other;
+			plat_x = other.transform.position.x;
+			plat_z = other.transform.position.z;
+			play_x = transform.position.x;
+			play_z = transform.position.z;
+
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.tag == "Platform") {
+			collider = null;
 		}
 	}
 
 	void Update() {
-		if (collider.gameObject.tag == "Platform") { 
-			transform.position = collider.transform.position;
+		if (collider != null) {
+			if (collider.gameObject.tag == "Platform") { 
+				plat_change_x = collider.transform.position.x - plat_x;
+				plat_change_z = collider.transform.position.z - plat_z;
+
+				Debug.Log (plat_change_x);
+				Debug.Log (plat_change_z);
+
+				float x = play_x + plat_change_x;
+				float z = play_z + plat_change_z;
+				transform.position = new Vector3 (x, transform.position.y, z);
+			}
 		}
 	}
 
