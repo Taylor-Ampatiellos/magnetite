80b977644e937e91d8c186b4d4220e09e9dfa69a
Chris Hinstorff <chinst01@eecs.tufts.edu>
2016-12-14 15:44:37 -0500

    Add Door Sounds prefab for button/door sounds

---
+++ Unity/Magnetite/Assets/_scripts/DoorSounds.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorSounds : MonoBehaviour
+{
+
+	public AudioClip buttonSound;
+	public AudioClip doorSound;
+
+	void Start ()
+	{
+		DoorController[] dcs = FindObjectsOfType (typeof(DoorController)) as DoorController[];
+		foreach (DoorController dc in dcs) {
+			dc.buttonAudioSource = dc.gameObject.AddComponent<AudioSource> ();
+			dc.buttonAudioSource.clip = buttonSound;
+
+			dc.doorAudioSource = dc.gameObject.AddComponent<AudioSource> ();
+			dc.doorAudioSource.clip = doorSound;
+		}
+	}
+}
