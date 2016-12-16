add65043abc3f8d2cf025dda9e4f35fc7f7be4c2
Taylor Ampatiellos <Taylor1VT@comcast.net>
2016-12-16 16:38:49 -0500

    More Fixes

--- Unity/Magnetite/Assets/Scripts/DoorSounds.cs
+++ Unity/Magnetite/Assets/Scripts/DoorSounds.cs
@@ -11,8 +11,8 @@ public class DoorSounds : MonoBehaviour
 	{
 		DoorController[] dcs = FindObjectsOfType (typeof(DoorController)) as DoorController[];
 		foreach (DoorController dc in dcs) {
-			dc.buttonAudioSource = dc.gameObject.AddComponent<AudioSource> ();
-			dc.buttonAudioSource.clip = buttonSound;
+			dc.doorAudioSource = dc.gameObject.AddComponent<AudioSource> ();
+			dc.doorAudioSource.clip = buttonSound;
 
 			dc.doorAudioSource = dc.gameObject.AddComponent<AudioSource> ();
 			dc.doorAudioSource.clip = doorSound;
