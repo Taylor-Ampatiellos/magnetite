8277f65ea42ec84384b3c05252e155803b43bd66
Taylor Ampatiellos <Taylor1VT@comcast.net>
2016-12-16 16:21:34 -0500

    Text That Disappears

---
+++ Unity/Magnetite/Assets/Scripts/TextTrig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTrig : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			Destroy(GameObject.FindGameObjectWithTag ("text"));
+		}
+	}
+}
